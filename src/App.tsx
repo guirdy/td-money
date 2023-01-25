b69968e54e44ccce9bb0d22e505f9107935287e6
guirdy <guilherme.gl1997@gmail.com>
2023-01-25 10:40:33 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -7,8 +8,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-
-      <h1>Hello World</h1>
+      <Transactions />
    </ThemeProvider>
   )
 }
